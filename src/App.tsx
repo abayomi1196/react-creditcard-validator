8cba9bc75e4815a73e7c5adc9f748655dbdb3ba7
Abayomi Ayodele <samabayomi301@gmail.com>
2022-03-15 13:13:09 +0100

    style: remove inline styles

--- src/App.tsx
+++ src/App.tsx
@@ -16,40 +16,31 @@ function App() {
     <div className='App'>
       <h1>react-creditcard-input</h1>
 
-      <div style={{ marginBottom: "1em" }}>
+      <div>
         <input
           type='text'
           {...getCardNumberProps()}
           data-testid='cardNumberEl'
         />
-        <small
-          style={{ color: "red", display: "block" }}
-          data-testid='cardNumberErrorEl'
-        >
+        <small data-testid='cardNumberErrorEl'>
           {erroredInputs.cardNumber && erroredInputs.cardNumber}
         </small>
       </div>
 
-      <div style={{ marginBottom: "1em" }}>
+      <div>
         <input
           type='text'
           {...getExpiryDateProps()}
           data-testid='expiryDateEl'
         />
-        <small
-          style={{ color: "red", display: "block" }}
-          data-testid='expiryErrorEl'
-        >
+        <small data-testid='expiryErrorEl'>
           {erroredInputs.expiryDate && erroredInputs.expiryDate}
         </small>
       </div>
 
       <div>
         <input type='text' {...getCVCProps()} data-testid='cvcEl' />
-        <small
-          style={{ color: "red", display: "block" }}
-          data-testid='cvcErrorEl'
-        >
+        <small data-testid='cvcErrorEl'>
           {erroredInputs.cvc && erroredInputs.cvc}
         </small>
       </div>
