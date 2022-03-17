3116788e06d8ef5d5149a7918b854ba814215c9a
JoshX [MacBookPro] <joshua.osagie01@gmail.com>
2022-03-17 10:27:02 +0100

    ➕🔨chore(lint): configure linter/formatter

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,25 +1,22 @@
-import { getCardTypeByValue } from "./cardTypes";
+import { getCardTypeByValue } from './cardTypes';
 
 const MONTH_REGEX = /(0[1-9]|1[0-2])/;
 
-export const EMPTY_CARD_NUMBER = "Enter a card number";
-export const EMPTY_EXPIRY_DATE = "Enter an expiry date";
-export const EMPTY_CVC = "Enter a CVC";
+export const EMPTY_CARD_NUMBER = 'Enter a card number';
+export const EMPTY_EXPIRY_DATE = 'Enter an expiry date';
+export const EMPTY_CVC = 'Enter a CVC';
 
-export const INVALID_CARD_NUMBER = "Card number is invalid";
-export const INVALID_EXPIRY_DATE = "Expiry date is invalid";
-export const INVALID_CVC = "CVC is invalid";
+export const INVALID_CARD_NUMBER = 'Card number is invalid';
+export const INVALID_EXPIRY_DATE = 'Expiry date is invalid';
+export const INVALID_CVC = 'CVC is invalid';
 
-export const MONTH_OUT_OF_RANGE = "Expiry month must be between 01 and 12";
-export const YEAR_OUT_OF_RANGE = "Expiry year cannot be in the past";
-export const DATE_OUT_OF_RANGE = "Expiry date cannot be in the past";
+export const MONTH_OUT_OF_RANGE = 'Expiry month must be between 01 and 12';
+export const YEAR_OUT_OF_RANGE = 'Expiry year cannot be in the past';
+export const DATE_OUT_OF_RANGE = 'Expiry date cannot be in the past';
 
 export const hasCardNumberReachedMaxLength = (currentValue: string) => {
   const cardType = getCardTypeByValue(currentValue);
-  return (
-    cardType &&
-    currentValue.length >= cardType.lengths[cardType.lengths.length - 1]
-  );
+  return cardType && currentValue.length >= cardType.lengths[cardType.lengths.length - 1];
 };
 
 // https://en.wikipedia.org/wiki/Luhn_algorithm
@@ -27,7 +24,7 @@ export const hasCardNumberReachedMaxLength = (currentValue: string) => {
 export const validateLuhn = (cardNumber: string) => {
   return (
     cardNumber
-      .split("")
+      .split('')
       .reverse()
       .map((digit) => parseInt(digit, 10))
       .map((digit, idx) => (idx % 2 ? digit * 2 : digit))
@@ -38,22 +35,17 @@ export const validateLuhn = (cardNumber: string) => {
   );
 };
 
-export const getCardNumberError = (
-  cardNumber: string,
-  { errorMessages = {} as any } = {}
-) => {
+export const getCardNumberError = (cardNumber: string, { errorMessages = {} as any } = {}) => {
   if (!cardNumber) {
     return errorMessages.emptyCardNumber || EMPTY_CARD_NUMBER;
   }
 
-  const rawCardNumber = cardNumber.replace(/\s/g, "");
+  const rawCardNumber = cardNumber.replace(/\s/g, '');
 
   const cardType = getCardTypeByValue(rawCardNumber);
 
   if (cardType && cardType.lengths) {
-    const doesCardNumberMatchLength = cardType.lengths.includes(
-      rawCardNumber.length
-    );
+    const doesCardNumberMatchLength = cardType.lengths.includes(rawCardNumber.length);
     if (doesCardNumberMatchLength) {
       const isLuhnValid = validateLuhn(rawCardNumber);
       return isLuhnValid;
@@ -62,15 +54,12 @@ export const getCardNumberError = (
   return errorMessages.invalidCardNumber || INVALID_CARD_NUMBER;
 };
 
-export const getExpiryDateError = (
-  expiryDate: string,
-  { errorMessages = {} as any } = {}
-) => {
+export const getExpiryDateError = (expiryDate: string, { errorMessages = {} as any } = {}) => {
   if (!expiryDate) {
     return errorMessages.emptyExpiryDate || EMPTY_EXPIRY_DATE;
   }
 
-  const rawExpiryDate = expiryDate.replace(" / ", "").replace("/", "");
+  const rawExpiryDate = expiryDate.replace(' / ', '').replace('/', '');
 
   if (rawExpiryDate.length === 4) {
     const month = rawExpiryDate.slice(0, 2);
@@ -81,10 +70,7 @@ export const getExpiryDateError = (
     if (parseInt(year) < new Date().getFullYear()) {
       return errorMessages.yearOutOfRange || YEAR_OUT_OF_RANGE;
     }
-    if (
-      parseInt(year) === new Date().getFullYear() &&
-      parseInt(month) < new Date().getMonth() + 1
-    ) {
+    if (parseInt(year) === new Date().getFullYear() && parseInt(month) < new Date().getMonth() + 1) {
       return errorMessages.dateOutOfRange || DATE_OUT_OF_RANGE;
     }
 
@@ -93,10 +79,7 @@ export const getExpiryDateError = (
   return errorMessages.invalidExpiryDate || INVALID_EXPIRY_DATE;
 };
 
-export const getCVCError = (
-  cvc: string,
-  { errorMessages = {} as any } = {}
-) => {
+export const getCVCError = (cvc: string, { errorMessages = {} as any } = {}) => {
   if (!cvc) {
     return errorMessages.emptyCVC || EMPTY_CVC;
   }
